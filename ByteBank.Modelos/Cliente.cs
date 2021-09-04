d896a244df1bb86ab46b5495c8b339e91c0c3944
Vinicius Martins <vm3090474@gmail.com>
2021-09-04 17:28:55 -0300

    Copiando projeto anterior

---
+++ ByteBank.Modelos/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Modelos
+{
+    public class Cliente
+    {
+        public string Nome { get; set; }
+
+        public string CPF { get; set; }
+        public string Profissao { get; set; }
+    }
+}
---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dataFimPagamento = new DateTime(2021, 12, 17);
+            DateTime dataCorrente = DateTime.Now;
+
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            //string mensagem = "Vencimento em " + Humanizer.TimeSpanHumanizerExtensions.Humanize(diferenca);
+            
+            //Console.WriteLine(mensagem);
+            Console.WriteLine(dataCorrente);
+            Console.WriteLine(dataFimPagamento);
+
+        }
+
+        static string GetIntervaloDeTempoLegivel(TimeSpan timeSpan)
+        {
+            if(timeSpan.Days > 30)
+            {
+                int quantidadeMeses = timeSpan.Days / 30;
+                if(quantidadeMeses == 1)
+                {
+                    return " 1 mês";
+                }
+                return quantidadeMeses + " meses";
+            }
+
+            return timeSpan.Days + " dias";
+        }
+    }
+}
